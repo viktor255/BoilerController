3380a0551cac4d43fa6e93b632b9716f1f476b17
Viktor Lehotský <viktor.lehotsky@artin.cz>
2020-05-03 20:21:45 +0200

    project init

---
+++ app/src/main/java/cz/muni/fi/pv239/boilercontroller/MainActivity.kt
@@ -0,0 +1,12 @@
+package cz.muni.fi.pv239.boilercontroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
